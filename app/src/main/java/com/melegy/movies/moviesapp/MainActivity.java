187b8ea3df4f6c2247c77b271f4cf813e5be6ec1
Ahmad <ahmad.melegy@gmail.com>
2015-08-15 03:08:47 +0200

    Add Settings Activity

--- app/src/main/java/com/melegy/movies/moviesapp/MainActivity.java
+++ app/src/main/java/com/melegy/movies/moviesapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.melegy.movies.moviesapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -30,6 +31,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
