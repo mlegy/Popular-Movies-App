387270df0b82397d1c37614479f4cfec8a2a7c0d
Ahmad <ahmad.melegy@gmail.com>
2015-09-28 02:24:21 +0200

    Replace Actionbar with Toolbar.

--- app/src/main/java/com/melegy/movies/moviesapp/UI/MainActivity.java
+++ app/src/main/java/com/melegy/movies/moviesapp/UI/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.util.Pair;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -17,11 +18,15 @@ public class MainActivity extends ActionBarActivity implements MainActivityFragm
 
     private static final String TAG_FRAGMENT = "MOVIE_FRAGMENT";
     private boolean mTwoPane;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);                   // Setting toolbar as the ActionBar with setSupportActionBar() call
 
         if (findViewById(R.id.movie_detail_container) != null) {
             mTwoPane = true;
